b8642c118a1858f12d28e3530092ef48cf405e87
vivak <vk2853@gmail.com>
2019-04-28 16:28:42 +0530

    indentation fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  	resources :gateways, only: [] do
-  		collection do
-    		post :message_generator
-    		post :transaction
-  	end
+	resources :gateways, only: [] do
+		collection do
+  		post :message_generator
+  		post :transaction
+		end
 	end
-  
 end
